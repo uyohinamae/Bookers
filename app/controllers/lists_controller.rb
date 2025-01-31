3e39e3bcbde1276e69d0d59a132419177fc6a8c3
EC2 Default User <ec2-user@ip-172-31-3-154.ap-northeast-1.compute.internal>
2025-01-31 12:39:00 +0000

    end 1-10

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,12 +4,20 @@ class ListsController < ApplicationController
     @list = List.new  
   end
 
+  
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
+  end
+
   def create
-    list = List.new(list_params)
-    list.save
-    # redirect_to '/top' を削除して、以下コードに変更
-    # 詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+     render :new
+    end  
   end
 
   def index
