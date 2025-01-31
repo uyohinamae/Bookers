9352ccb1209b095f0ec4c64f2ba7433045542341
EC2 Default User <ec2-user@ip-172-31-3-154.ap-northeast-1.compute.internal>
2025-01-31 06:58:01 +0000

    Initial Commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,38 @@
+class ListsController < ApplicationController
+  def new
+     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new  
+  end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    # redirect_to '/top' を削除して、以下コードに変更
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)
+  end
+
+  def index
+    @lists = List.all  
+  end
+
+  def show
+    @list = List.find(params[:id])  
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
+  end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+end
